42213fe7d349f75723c12a878b94b2d296788e86
bcohn12 <bcohn12@gmail.com>
2017-02-21 16:39:29 -0800

    consoidated some alumni

--- Alumni.php
+++ Alumni.php
@@ -1,715 +1,184 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
 <head>
 <title>The Brain-Body Dynamics Lab: Alumni</title>
-
 <?php include("TemplateUpperSectionLab.php"); ?>
-
-
 <table class="peopletable" align="center">
-
-<tr>
 <td colspan="2">
 <div class="memberSeparator">
 Alumni of the USC BBDL
 </div>
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Hannah.php">
 <img class="memberimg" src="pictures/Hannah.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Hannah.php"> Na-hyeon (Hannah) Ko </a>
-</td>
-</tr>
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Emily.php">
 <img class="memberimg" src="pictures/Emily.jpg" alt="Emily image" title="Emily" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Emily.php"> Emily Lawrence </a>
-</td>
-</tr>
-<tr>
-<td class="memberImgCol">
-<a class="centerlink" href="">
 <img class="memberimg" src="pictures/questionmark.jpg" alt="Meng image" title="Meng" />
 </a>
-</td>
-
-<td class="memberNameCol">
-<a class="centerlink" href=""> Meng Fen Tsai</a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
-<a class="centerlink" href="">
+ Meng Fen Tsai</a>
 <img class="memberimg" src="pictures/questionmark.jpg" alt="Benjamin image" title="Benjamin" />
 </a>
-</td>
-
-<td class="memberNameCol">
-<a class="centerlink" href=""> Benjamin Dourthe </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
+ Benjamin Dourthe </a>
 <a class="centerlink" href="Alex.php">
 <img class="memberimg" src="pictures/Alex2.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Alex.php"> Alexander Reyes </a>
-</td>
-</tr>
-
-
-<tr>
-<td class="memberImgCol">
-<a class="centerlink" href="">
 <img class="memberimg" src="pictures/Lorenzo.jpg" alt="Lorenzo image" title="Lorenzo" />
 </a>
-</td>
-
-<td class="memberNameCol">
-<a class="centerlink" href=""> Lorenzo Peppoloni </a>
-</td>
-</tr>
-
-
-<tr>
-<td class="memberImgCol">
+ Lorenzo Peppoloni </a>
 <a class="centerlink" href="Karen.php">
 <img class="memberimg" src="pictures/Karen.jpg" alt="Karen image" title="Karen" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Karen.php"> Karen Poblete  </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Victor.php">
 <img class="memberimg" src="pictures/Victor.jpg" alt="Victor image" title="Victor" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Victor.php"> Victor Barradas </a>
-</td>
-</tr>
-
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Sarine.php">
 <img class="memberimg" src="pictures/sarine.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Sarine.php"> Sarine Babikian </a>
-</td>
-</tr>
-
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Christoff.php">
 <img class="memberimg" src="pictures/Christoff.jpg" />
 </a>
-</td>
-<td class="memberNameCol">
 <a class="centerlink" href="Christoff.php"> Christoff Sulzenbacher</a>
-</td>
-</tr>
-
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Yves.php">
 <img class="memberimg" src="pictures/Yves.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Yves.php"> Yves Nazon III</a>
-</td>
-</tr>
-
-
-
-
-
-<tr>
-<td class="memberImgCol">
-<a class="centerlink" href="">
 <img class="memberimg" src="pictures/MarkLyle.jpeg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="People.php"> Mark Lyle </a>
-</td>
-</tr>
-
-
-
-<tr>
-<td class="memberImgCol">
-<a class="centerlink" href="">
 <img class="memberimg" src="pictures/KangWooLee.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="KangWooLee.php"> KangWoo Lee </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
-<a class="centerlink" href="">
 <img class="memberimg" src="pictures/John.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="John.php"> John Rocamora </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Brendan.php">
 <img class="memberimg" src="pictures/Brendan.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Brendan.php"> Brendan Holt </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
-<a class="centerlink" href="">
 <img class="memberimg" src="pictures/Srideep.jpg" alt="Srideep image" title="Srideep" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Srideep.php"> Srideep Musuvathy </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="http://www.linkedin.com/in/manishkurse">
 <img class="memberimg" src="pictures/Manish.JPG" alt="Manish image" title="Manish" /> 
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="http://www.linkedin.com/in/manishkurse"> Manish Kurse </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Sudarshan.php">
 <img class="memberimg" src="pictures/Sudarshan.JPG" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Sudarshan.php"> Sudarshan Dayanidhi </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Kornelius.php">
 <img class="memberimg" src="pictures/AkakaCropped.jpg" alt="Kornelius image" title="Kornelius" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Kornelius.php"> Kornelius R&aacute;cz <br /> (Cornelius Raths) </a>
-</td>
-</tr>
-
-
-
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Josh.php">
 <img class="memberimg" src="pictures/Josh.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Josh.php"> Josh Inouye </a>
-</td>
-</tr>
-
-
-</td>
-</tr>
-
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Nora.php">
 <img class="memberimg" src="pictures/NoraCropped.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Nora.php"> Nora Nelson </a>
-</td>
-</tr>
-
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Youngmin.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B>Youngmin Oh</B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Novalie.JPG" />
-</td>
-
-<td class="memberNameCol">
 <B>Novalie Lilja</B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Isak.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B>Isak H&auml;gg</B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Devin.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Devin von Stade </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Kevin VO.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Kevin Varela O'Hara </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Stephanie.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Stephanie Davalos </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Irving Elyasib Santiago Catzim </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
-<a class="centerlink" href="">
 <img class="memberimg" src="pictures/questionmark.jpg" alt="Marta image" title="Marta" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <B> Marta Covadonga Mora Aguilar <B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Corey.php">
 <img class="memberimg" src="pictures/Corey.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Corey.php"> Corey Baker </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Jonathan.php">
 <img class="memberimg" src="pictures/jhu_pic.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Jonathan.php"> Jonathan Weisz </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Heiko.php">
 <img class="memberimg" src="pictures/heiko.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Heiko.php"> Heiko Hoffmann </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Cecilia.JPG" />
-</td>
-
-<td class="memberNameCol">
 <B> Cecilia Jou </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Vy.JPG" />
-</td>
-
-<td class="memberNameCol">
 <B> Vy Vo </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> George Tsianos </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Santosh.png" />
-</td>
-
-<td class="memberNameCol">
 <B> Santosh Shah </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Weiwei.php">
 <img class="memberimg" src="pictures/weiwei.jpg" alt="Weiwei image" title="Weiwei" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Weiwei.php"> Weiwei Li </a>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Kirk.JPG" />
-</td>
-
-<td class="memberNameCol">
 <B> Kirk Nichols </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Kari.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Kari Oki </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Lolly.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Alexandra Simoni </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/Robert.jpg"/>
-</td>
-
-<td class="memberNameCol">
 <B> Robert Taft </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <a class="centerlink" href="Melissa.php">
 <img class="memberimg" src="pictures/Melissa.jpg" />
 </a>
-</td>
-
-<td class="memberNameCol">
 <a class="centerlink" href="Melissa.php"> Melissa Sajnani </a>
-</td>
-</tr>
-
-<tr>
-<td>
-
-</td>
-</tr>
-
-<tr>
 <td colspan="2">
 <div class="memberSeparator">
 Alumni of the Cornell NMBL
 </div>
-
-</td>
-</tr>
-
-
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/NathanWilson.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B>Nathan Wilson</B>
 <br />
 MS 2000
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Becky Avrin Zifchock, PhD </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/robertmcnamara.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Robert McNamara </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/anupam.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Anupam Saxena </B>
-</td>
-</tr>
-
-
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/alicia.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Flor Alicia Medina Vel&aacute;zquez </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/MichaelSherback.JPG" />
-</td>
-
-<td class="memberNameCol">
 <B> Michael Sherback </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/DanielBrown.JPG" />
-</td>
-
-<td class="memberNameCol">
 <B> Daniel Brown </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/BrendaChen.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Brenda Chen </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Karen Chin </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Tumay Kanar </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Shannon Miller </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/WilliamSeidel.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> William Seidel </B>
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Kirana Ramakrishna </B> <br /><br />
 MAE Class of '01
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Xiaozheng Zhong </B> <br /><br />
 MEng Class of '01, <br /> at Amazon.com 
-</td>
-</tr>
-
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Matthew Schmucker </B> <br /><br />
 MAE Class of '01
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
 <img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Michal Weisman </B> <br /><br />
 MAE Class of '02, MEng Class of '02
-</td>
-</tr>
-
-<tr>
-<td class="memberImgCol">
-<img class="memberimg" src="pictures/questionmark.jpg" />
-</td>
-
-<td class="memberNameCol">
 <B> Paul Menno Scholten </B> <br /><br />
 MAE Class of '02
-</td>
-</tr>
-
-</table>
-
-<br />
-<br />
-<div class="center">
-<a class="centerlink" href="Alumni2.php"> Next </a>
-</div>
-
-<?php include("TemplateLower.php"); ?>
