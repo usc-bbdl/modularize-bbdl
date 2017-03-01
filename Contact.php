598b6a25c2affb406601aa0b7abbd6bbb04fe714
bcohn12 <bcohn12@gmail.com>
2017-03-01 14:39:36 -0800

    rm extra php files

--- Contact.php
+++
@@ -1,10 +0,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><title>The Brain-Body Dynamics Lab: Contact & Directions</title><?php include("TemplateUpperSectionLab.php"); ?><table class="contactTable" cellspacing="0">
-
-<tr>
-<td>
-<div class="center"><h4>Contact Information</h4>
-</div>
-<br />
-</td>
-</tr>
-<tr><td><img class="rthimage" src="pictures/rth.jpg" alt="Ronald Tutor Hall" /></td></tr><tr><td><p><strong> Brain-Body Dynamics Lab </strong><br />Ronald Tutor Hall, RTH-421 <br />3710 S. McClintock Ave <br />Los Angeles, CA 90089-2905, USA <br /> Phone: (213) 740 4219 <br />Fax: (213) 821 5696 <br /><br /></p></td></tr><tr><td><strong> Directions to the lab </strong><br /><br /></td></tr><tr><td><a href="http://maps.google.com/maps?f=q&hl=en&q=3710+McClintock+Ave,+Los+Angeles,+CA+90089&ie=UTF8&ll=34.02019,-118.290482&spn=0.016647,0.041628&om=1"><img class="rthimage" src="pictures/mapToLab.png" alt"Map to Lab" /></a><br /><br /><a class="centerlink" href="http://maps.google.com/maps?f=q&hl=en&q=3710+McClintock+Ave,+Los+Angeles,+CA+90089&ie=UTF8&ll=34.02019,-118.290482&spn=0.016647,0.041628&om=1/"> Locate the lab at Google Maps </a><br /><br /><a class="centerlink" href="Instructions.php">Driving to the lab: Instructions</a></td></tr></table><?php include("TemplateLower.php"); ?>
